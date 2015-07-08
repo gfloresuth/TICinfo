6c06e7cbbb6fea125a658a893b9c4df0a08610b1
Gustavo Flores (darkuskyo) <gflores_uth@outlook.com>
2015-07-08 13:24:22 -0700

    Nuevos cambios
    Nuevos cambios en menú y otras opciones.

--- js/index.js
+++ js/index.js
@@ -62,7 +62,7 @@ var app = {
         $('#scroller').html('Conectando con servidor...');
 		$.ajax({
 			dataType: 'json',
-			url: 'http://www.tic-uth.net/gflores/ticinfo/',
+			url: 'http://darkuskyo.net/apps/?firefoxos=yes',//'http://www.tic-uth.net/gflores/ticinfo/',
 			data: {}
 		}).done(function (data) {
             console.log(data);
